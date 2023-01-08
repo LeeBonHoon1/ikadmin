2075069d221cf2464bfbb07b0bd36830613e7611
LeeBonHoon <80507816+LeeBonHoon1@users.noreply.github.com>
2023-01-08 20:17:10 +0900

    라우트 수정

--- src/App.js
+++ src/App.js
@@ -11,50 +11,50 @@ import Product from "./pages/product/Product";
 import NewProduct from "./pages/newProduct/NewProduct";
 import Login from "./pages/login/Login";
 import { useSelector } from "react-redux";
-import { useEffect, useMemo } from "react";
 
 function App() {
   const isLoggedIn = useSelector((state) => !!state.user.email);
+  console.log(isLoggedIn);
 
   return (
     <>
-      {isLoggedIn ? (
-        <Router>
-          <Topbar />
-          <div className="container">
-            <Sidebar />
-            <Switch>
-              <Route exact path="/">
-                <Home />
-              </Route>
-              <Route path="/users">
-                <UserList />
-              </Route>
-              <Route path="/user/:userId">
-                <User />
-              </Route>
-              <Route path="/newUser">
-                <NewUser />
-              </Route>
-              <Route path="/group">
-                <Group />
-              </Route>
-              <Route path="/product/:productId">
-                <Product />
-              </Route>
-              <Route path="/newproduct">
-                <NewProduct />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      ) : (
-        <Router>
-          <Route path="/login">
+      <Router>
+        {isLoggedIn ? (
+          <>
+            <Topbar />
+            <div className="container">
+              <Sidebar />
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/users">
+                  <UserList />
+                </Route>
+                <Route path="/user/:userId">
+                  <User />
+                </Route>
+                <Route path="/newUser">
+                  <NewUser />
+                </Route>
+                <Route path="/group">
+                  <Group />
+                </Route>
+                <Route path="/product/:productId">
+                  <Product />
+                </Route>
+                <Route path="/newproduct">
+                  <NewProduct />
+                </Route>
+              </Switch>
+            </div>
+          </>
+        ) : (
+          <Route>
             <Login />
           </Route>
-        </Router>
-      )}
+        )}
+      </Router>
     </>
   );
 }
